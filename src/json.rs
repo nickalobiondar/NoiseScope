094fd2d0982e5d314d3635a970bb8cb536adc644
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2016-12-13 14:36:00 +0700

    feat: add tls

--- src/json.rs
+++ src/json.rs
@@ -342,3 +342,25 @@ impl<'a> Parser<'a> {
         let mut items = Vec::new();
         self.skip_ws();
         if self.peek() == Some(b']') {
+            self.pos += 1;
+            return Ok(Json::Arr(items));
+        }
+        loop {
+            let value = self.parse_value()?;
+            items.push(value);
+            self.skip_ws();
+            match self.peek() {
+                Some(b',') => {
+                    self.pos += 1;
+                    continue;
+                }
+                Some(b']') => {
+                    self.pos += 1;
+                    break;
+                }
+                _ => return Err(self.err("expected ',' or ']' in array")),
+            }
+        }
+        Ok(Json::Arr(items))
+    }
+
