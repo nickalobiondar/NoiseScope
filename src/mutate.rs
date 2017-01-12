fe63efaf3e0835b15505d1cf69899a04947dff85
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-01-12 11:20:00 +0700

    refactor: state machine

--- src/mutate.rs
+++ src/mutate.rs
@@ -26,3 +26,16 @@ use crate::model::Transcript;
 #[derive(Debug, Clone)]
 pub struct SplitMix64 {
     state: u64,
+}
+
+impl SplitMix64 {
+    pub fn new(seed: u64) -> Self {
+        SplitMix64 { state: seed }
+    }
+
+    pub fn next_u64(&mut self) -> u64 {
+        self.state = self.state.wrapping_add(0x9E37_79B9_7F4A_7C15);
+        let mut z = self.state;
+        z = (z ^ (z >> 30)).wrapping_mul(0xBF58_476D_1CE4_E5B9);
+        z = (z ^ (z >> 27)).wrapping_mul(0x94D0_49BB_1331_11EB);
+        z ^ (z >> 31)
