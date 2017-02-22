9a234e304a4b6dd87f2e75e53c1cc07b7edfc7ae
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-02-22 15:16:00 +0700

    chore: deterministic

--- src/minimize.rs
+++ src/minimize.rs
@@ -10,3 +10,15 @@
 //! plan still fails the predicate. It runs to a fixed point. This is O(n^2)
 //! predicate evaluations in the worst case but plans are short, and each
 //! evaluation is a deterministic replay.
+
+use crate::model::Transcript;
+use crate::mutate::{apply_all, Mutation};
+
+/// A predicate that returns `true` when a transcript is considered *failing*
+/// (i.e. it diverges from the spec in the way we are trying to preserve).
+pub type FailPredicate<'a> = dyn Fn(&Transcript) -> bool + 'a;
+
+/// Result of a minimization pass.
+#[derive(Debug, Clone)]
+pub struct Minimized {
+    pub plan: Vec<Mutation>,
