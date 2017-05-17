d2b6b2686f97ab5ccf9faf60819371ae7b141782
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-05-17 10:20:00 +0700

    feat: tls

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@
 //!
 //! A **cryptographic handshake transcript state-machine analyzer and fuzzer**.
 //!
+//! `noisescope` treats a handshake as a declarative finite state machine plus a
+//! set of structural metadata invariants (role, order, nonce freshness,
+//! sequence monotonicity). It replays observed *transcripts* against a *spec*,
+//! reports where they diverge, and can *fuzz* a conforming transcript with
+//! deterministic mutations (drop / duplicate / reorder / corrupt-metadata) to
