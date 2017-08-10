147b8ac0d9a8a1d1c63a19dbe819350d33bdddcf
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-08-10 13:21:00 +0700

    style: clean up rust cli

--- viewer/src/cli.ts
+++ viewer/src/cli.ts
@@ -4,3 +4,15 @@
  *
  * Reads a `noisescope paths` JSON document from a file argument or stdin and
  * renders it as ASCII (default) or SVG.
+ *
+ * Usage:
+ *   noisescope-viewer [--svg] [path.json]
+ *   noisescope paths spec.json transcript.json | noisescope-viewer --svg > path.svg
+ */
+
+import { readFileSync } from "node:fs";
+import { parsePathDoc, renderAscii, renderSvg } from "./render.js";
+
+function readStdin(): string {
+  try {
+    return readFileSync(0, "utf8");
