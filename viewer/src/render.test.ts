948d5a67990b8c6135506d7c458845c0dbb7026d
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-08-17 10:50:00 +0700

    docs: expand protocol testing

--- viewer/src/render.test.ts
+++ viewer/src/render.test.ts
@@ -19,3 +19,17 @@ const sample: PathDoc = {
   accepting: ["established"],
   states: ["await_e", "await_ee", "await_se", "established"],
   final_state: "established",
+  reached_accepting: true,
+  path: [
+    { event_index: 0, event: "initiator:e#1001", from: "await_e", to: "await_ee" },
+    { event_index: 1, event: "responder:e_ee_s_es#2002", from: "await_ee", to: "await_se" },
+    { event_index: 2, event: "initiator:s_se", from: "await_se", to: "established" },
+  ],
+};
+
+test("isPathDoc accepts valid docs", () => {
+  assert.equal(isPathDoc(sample), true);
+});
+
+test("isPathDoc rejects garbage", () => {
+  assert.equal(isPathDoc({ protocol: 1 }), false);
