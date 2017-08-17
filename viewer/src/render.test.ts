c790d97b88590fd05cd17cc630945b94c2fcac94
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-08-17 10:25:00 +0700

    style: clean up state machine

--- viewer/src/render.test.ts
+++ viewer/src/render.test.ts
@@ -6,3 +6,16 @@
 import { test } from "node:test";
 import assert from "node:assert/strict";
 import {
+  isPathDoc,
+  parsePathDoc,
+  renderAscii,
+  renderSvg,
+  type PathDoc,
+} from "./render.js";
+
+const sample: PathDoc = {
+  protocol: "noise-XX-abstract",
+  initial: "await_e",
+  accepting: ["established"],
+  states: ["await_e", "await_ee", "await_se", "established"],
+  final_state: "established",
