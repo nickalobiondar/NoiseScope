3115f6d2b963469b866e583492943e8f010abd98
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-08-17 10:07:00 +0700

    fix: correct state machine

---
+++ viewer/src/render.test.ts
@@ -0,0 +1,8 @@
+/**
+ * Tests for the noisescope-viewer render module, using the built-in node:test
+ * runner (no third-party test framework required).
+ */
+
+import { test } from "node:test";
+import assert from "node:assert/strict";
+import {
