b3e0521851fafb0056566cea02a7736216af7c14
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2017-09-19 18:15:00 +0700

    chore: tls

--- tests/integration.rs
+++ tests/integration.rs
@@ -9,3 +9,19 @@ use std::path::PathBuf;
 use noisescope::divergence::{fuzz, report_json, FuzzConfig};
 use noisescope::engine::{replay, ViolationKind};
 use noisescope::mutate::apply_all;
+use noisescope::parser::{parse_protocol, parse_transcript};
+
+fn fixture(name: &str) -> String {
+    let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    p.push("fixtures");
+    p.push(name);
+    fs::read_to_string(&p).unwrap_or_else(|e| panic!("cannot read fixture {name}: {e}"))
+}
+
+fn spec(name: &str) -> noisescope::model::ProtocolSpec {
+    parse_protocol(&fixture(name)).expect("spec parses")
+}
+
+fn transcript(name: &str) -> noisescope::model::Transcript {
+    parse_transcript(&fixture(name)).expect("transcript parses")
+}
